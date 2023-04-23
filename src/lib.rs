9028efffdd051e29b0bacc77cd6d5e5aa2edf3a5
b-chase <bcrawfc@gmail.com>
2023-04-22 22:34:45 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,6 @@ use std::{fs::File, fmt::format};
 use serde::{Serialize, Deserialize};
 use bincode::{config, serialize, deserialize};
 
-const EMBED_LEN : usize = 1536;
 
 fn dot_product(a:&Vec<f64>, b:&Vec<f64>) -> f64 {
     a.iter()
@@ -14,8 +13,8 @@ fn dot_product(a:&Vec<f64>, b:&Vec<f64>) -> f64 {
 }
 
 /// Memory struct for memories to be stored
-#[derive(Clone)]
 #[pyclass]
+#[derive(Clone)]
 struct Memory {
     text: String,
     embed_size: usize, 
@@ -30,12 +29,14 @@ impl Memory {
         let resize_to = embedding_size.unwrap_or(embed_vector.len());
         let mut fixed_embedding = embed_vector.clone();
         fixed_embedding.resize_with(resize_to, Default::default);
-        return Memory {
+        let mag = dot_product(&fixed_embedding, &fixed_embedding).powf(0.5);
+
+        Memory {
             text: text, 
             embed_size: resize_to, 
             embedding: fixed_embedding, 
-            magnitude: dot_product(&fixed_embedding, &fixed_embedding).powf(0.5)
-        };
+            magnitude: mag
+        }
     }
 
     fn __repr__(&self) -> String {
@@ -62,17 +63,17 @@ impl Memory {
 #[pyclass(get_all, dict, sequence)]
 struct MemoryStore {
     memories: Vec<Memory>, 
-    embed_len: usize
+    embedding_size: usize
 }
 
 #[pymethods]
 impl MemoryStore {
     #[new]
-    fn new(embedding_length: usize, initial_memories: Option<PyList>) -> Self {
+    fn new(embedding_length: usize, initial_memories: Option<Vec<Memory>>) -> Self {
         // if initial_memories
         MemoryStore { 
-            memories: Vec::new(), 
-            embed_len: embedding_length
+            memories: initial_memories.unwrap_or(Vec::new()), 
+            embedding_size: embedding_length
         }
     }
 
@@ -81,37 +82,27 @@ impl MemoryStore {
     }
 
     #[pyo3(signature=(*args))]
-    fn add_memories(&mut self, args: PyList) {
+    fn add_memories(&mut self, args: Vec<Memory>) {
         // let new_memory = Memory::new(text, numbers);
-        for i in 0..(args.len().unwrap_or(0)) {
-            let new_mem = args.get_item(i).unwrap();
-            self.memories.push(new_mem.extract().unwrap());
-        }
+        self.memories.extend_from_slice(args.as_slice());
     }
 
     fn top_n_matches(&mut self, query: &Memory, n: usize, must_include_text: Option<&str>) -> Vec<(f64,Memory)> {
-        
-        let mut dist_index_pairs = self.memories.par_iter()
+        /*
+            Returns the top 'n' Memories by cosine similarity of their embedding vectors to the supplied 'query' memory.
+            Optionally include a must-have text string, 'must_include_text', to filter results before searching.
+         */
+        let mut dist_mem_pairs = self.memories.par_iter()
             .filter_map(|memory| {
-            if memory.text.contains(must_include_text.unwrap_or_default()) {
-                Some(query.compare(memory), memory.to_owned())
-            } else { None }
-            })
-            // .map(|(index, memory)| (memory.compare(query), ))
-            .collect::<Vec<(f64, Memory)>>();
-        dist_index_pairs.sort_by_key(|(dist, index)| -(1e6*dist) as i32);
+                if must_include_text.is_none() ||
+                memory.text.to_lowercase().contains(&(must_include_text.unwrap().to_lowercase())) {
+                    Some((query.compare(memory), memory.clone()))
+                } else {None}
+            }).collect::<Vec<(f64, Memory)>>();
 
-        if let Some(text) = must_include_text {
-            let mut filtered_pairs = Vec::new();
-            for (distance, index) in dist_index_pairs {
-                if memory.memory_json.contains(text) {
-                    filtered_pairs.push((distance, memory));
-                }
-            }
-            dist_pairs = filtered_pairs;
-        }
+            dist_mem_pairs.sort_by_key(|(dist, _mem)| -(1e6*dist) as i32);
 
-        dist_pairs[0..n].to_vec()
+            dist_mem_pairs[0..(n.min(dist_mem_pairs.len()))].to_vec()
     }
 
 }
